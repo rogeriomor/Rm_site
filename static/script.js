dea34e4d2e6b3343d4edc08300a3d48aaa8209a7
rogerio_mor <rogeriol17q2@gmail>
2022-02-03 21:37:55 -0300

    comit antes da aula 7

--- static/script.js
+++ static/script.js
@@ -9,3 +9,19 @@ menuMobile.addEventListener('click', () => {
         : menuMobile.classList.replace("bi-x","bi-list");
         body.classList.toggle("menu-nav-active");
 });
+
+/* Fechar menu quando clicar em algum item e muda o icone para list*/
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x","bi-list");
+        }
+    })
+})
+
+
+console.log(navItem)
